12a29029608f8949b894cb4643f86346774f8276
ticn <61242872+ticn@users.noreply.github.com>
2021-01-14 18:09:29 +0800

    Update jd_nh.js

--- jd_nh.js
+++ jd_nh.js
@@ -105,7 +105,7 @@ async function jdNh() {
 
 function getShareCode() {
   return new Promise(resolve => {
-    $.get({url:'https://gitee.com/shylocks/updateTeam/raw/main/jd_nh.json',headers:{
+    $.get({url:'https://raw.githubusercontent.com/ticn/linux/main/jd_nh.json',headers:{
         'user-agent': 'JD4iPhone/167490 (iPhone; iOS 14.2; Scale/3.00)'
       }},(err,resp,data)=>{
       try {
